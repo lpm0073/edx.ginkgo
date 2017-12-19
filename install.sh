30c0f5adb292dc261b6e3e12e8cfbcd14f433a9e
Lawrence McDaniel <lpm0073@gmail.com>
2017-12-19 17:02:10 -0600

    ginkgo.2 release

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ export LC_CTYPE="en_US.UTF-8"
 # 1. Set the OPENEDX_RELEASE variable:
 #export OPENEDX_RELEASE=open-release/ginkgo.1
 # Note: there are several small but really important bug fixes since the ginkgo.1 named release. i'm setting the releast to master (ie the most recent stable release) for the time being.
-export OPENEDX_RELEASE=master
+export OPENEDX_RELEASE=open-release/ginkgo.2
 
 
 
@@ -51,7 +51,7 @@ export OPENEDX_RELEASE=master
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo bash
 
 # 3. (Optional) If this is a new installation, randomize the passwords:
-wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
+#wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
 
 # 4. Install Open edX:
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/sandbox.sh -O - | bash > install.out
